0c0c35ba52a2f32e79a4fab355b6333b15b9b63c
Avery Lamp <averylamp@gmail.com>
2016-01-28 00:33:10 -0500

    Initial Commit

---
+++ Avery Lamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Avery Lamp
+//
+//  Created by Avery Lamp on 1/28/16.
+//  Copyright © 2016 Avery Lamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Avery LampTests/Avery_LampTests.swift
@@ -0,0 +1,36 @@
+//
+//  Avery_LampTests.swift
+//  Avery LampTests
+//
+//  Created by Avery Lamp on 1/28/16.
+//  Copyright © 2016 Avery Lamp. All rights reserved.
+//
+
+import XCTest
+@testable import Avery_Lamp
+
+class Avery_LampTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
