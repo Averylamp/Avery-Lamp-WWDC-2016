712fb3905c281f1f82a22b7fd6ece1bca126bc3b
Avery Lamp <averylamp@gmail.com>
2016-01-29 02:59:52 -0500

    Amazing transition achieved

--- Avery Lamp/SplashViewController.swift
+++ Avery Lamp/SplashViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    var continuable = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let bg = UIImageView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
@@ -21,56 +23,47 @@ class SplashViewController: UIViewController {
         bg.alpha = 0.0
         
         let bg2 = UIImageView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
-        bg2.image = UIImage(named: "NewYorkBackground")
+        
+        bg2.image = UIImage(named: "NewYorkBackground3")
         bg2.contentMode = UIViewContentMode.ScaleAspectFill
         self.view.addSubview(bg2)
         bg2.alpha = 0.0
-
+        
         let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = bg2.bounds
-        bg2.addSubview(blurEffectView)
-        
-        // Vibrancy Effect
-        
-        let bg3 = UIImageView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
-        bg3.image = UIImage(named: "NewYorkBackground2")
-        bg3.contentMode = UIViewContentMode.ScaleAspectFill
+        blurEffectView.alpha = 0.0
+        self.view.addSubview(blurEffectView)
         
         
-        let vibrancyEffect = UIVibrancyEffect(forBlurEffect: blurEffect)
-        let vibrancyEffectView = UIVisualEffectView(effect: vibrancyEffect)
-        vibrancyEffectView.frame = bg3.bounds
-        vibrancyEffectView.contentView.addSubview(bg3)
-        
-        blurEffectView.contentView.addSubview(vibrancyEffectView)
-        
-        UIView.animateWithDuration(0.5, animations: { () -> Void in
+        UIView.animateWithDuration(0.7, animations: { () -> Void in
             bg.alpha = 1.0
             }) { (finished) -> Void in
-                UIView.animateWithDuration(2, animations: { () -> Void in
+                UIView.animateWithDuration(4, animations: { () -> Void in
                     bg2.alpha = 1.0
+                    blurEffectView.alpha = 1.0
+                    bg.alpha = 1.0
                 })
         }
         
         let welcomeLabel  = UILabel(frame: CGRectMake(0,self.view.frame.height / 12 ,self.view.frame.width, 100))
-        welcomeLabel.text = "Welcome"
-        welcomeLabel.font = UIFont(name: "Panton-Thin", size: 30)
+        welcomeLabel.text = "Hello"
+        welcomeLabel.font = UIFont(name: "Panton-Light", size: 30)
         self.view.addSubview(welcomeLabel)
-        welcomeLabel.drawOutlineAnimatedWithLineWidth(0.5, withDuration: 1, fadeToLabel: false)
+        welcomeLabel.drawOutlineAnimatedWithLineWidth(0.5, withDuration: 3, fadeToLabel: true)
         
         
-        let myNameLabel  = UILabel(frame: CGRectMake(0,self.view.frame.height * 1 / 2  - 50 ,self.view.frame.width, 40))
+        let myNameLabel  = UILabel(frame: CGRectMake(0,self.view.frame.height * 1 / 2 ,self.view.frame.width, 40))
         myNameLabel.text = "My name is Avery"
-        myNameLabel.font = UIFont(name: "Panton-Thin", size: 20)
+        myNameLabel.font = UIFont(name: "Panton-Light", size: 20)
         self.view.addSubview(myNameLabel)
-        myNameLabel.drawOutlineAnimatedWithLineWidth(0.7, withDuration: 2, withDelay: 2, fadeToLabel: false);
+        myNameLabel.drawOutlineAnimatedWithLineWidth(0.7, withDuration: 2, withDelay: 4, fadeToLabel: true);
         
         
-        let picSize = myNameLabel.frame.origin.y - welcomeLabel.frame.origin.y - 100
+        let picSize = myNameLabel.frame.origin.y - welcomeLabel.frame.origin.y - welcomeLabel.frame.height - 50
         let proPic = UIImageView(frame: CGRectMake(0, 0, picSize, picSize))
-        proPic.image = UIImage(named: "Headshot")
-        proPic.center = CGPointMake(self.view.center.x, welcomeLabel.frame.origin.y + welcomeLabel.frame.height + picSize / 2 + 50)
+        proPic.image = UIImage(named: "Headshot2")
+        proPic.center = CGPointMake(self.view.center.x, welcomeLabel.frame.origin.y + welcomeLabel.frame.height + picSize / 2 + 25)
         proPic.layer.cornerRadius = picSize / 2
 //        proPic.layer.borderWidth = 1
         proPic.layer.masksToBounds = true
@@ -83,7 +76,7 @@ class SplashViewController: UIViewController {
         circle.position = CGPointMake(0, 0)
         circle.lineWidth = 0
         circle.fillColor = UIColor.clearColor().CGColor
-        circle.strokeColor = UIColor.blackColor().CGColor
+        circle.strokeColor = UIColor.whiteColor().CGColor
         proPic.layer.addSublayer(circle)
         
         let drawAnimation = CABasicAnimation(keyPath: "strokeEnd")
@@ -93,26 +86,33 @@ class SplashViewController: UIViewController {
         drawAnimation.toValue = NSNumber(float: 1.0)
         drawAnimation.timingFunction =  CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
         
-        delay(2.5) { () -> () in
-            circle.lineWidth = 2
+        delay(4.5) { () -> () in
+            circle.lineWidth = 3
             circle.addAnimation(drawAnimation, forKey: "drawCircleAnimation")
+            self.delay(3.5, closure: { () -> () in
+                self.continuable = true
+            })
         }
         
-        UIView.animateWithDuration(1.5, delay: 2, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+        UIView.animateWithDuration(2.5, delay: 4.5, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
             proPic.alpha = 1.0
             }) { (finished) -> Void in}
        
         let shimmerView = FBShimmeringView(frame: CGRectMake(0,self.view.frame.height * 5 / 6,self.view.frame.width, 60))
+        shimmerView.center = CGPointMake(self.view.center.x, self.view.frame.height * 5 / 6)
         shimmerView.alpha = 1.0
+        shimmerView.shimmeringSpeed = 100
+        shimmerView.shimmeringPauseDuration = 0.1
+        shimmerView.shimmeringHighlightLength = 0.5
         self.view.addSubview(shimmerView)
         let continueLabel = UILabel(frame: shimmerView.frame)
-        continueLabel.text = "Continue"
-        continueLabel.font = UIFont(name: "Panton-Light", size: 24)
+        continueLabel.text = "Tap Anywhere to Continue"
+        continueLabel.font = UIFont(name: "Panton-Regular", size: 24)
         continueLabel.textColor = UIColor.whiteColor()
         continueLabel.textAlignment = NSTextAlignment.Center
         shimmerView.contentView = continueLabel
         shimmerView.shimmering = true
-        continueLabel.drawOutlineAnimatedWithLineWidth(0.5, withDuration: 1, withDelay: 5, fadeToLabel: true)
+        continueLabel.drawOutlineAnimatedWithLineWidth(0.5, withDuration: 1, withDelay: 8, fadeToLabel: true)
         
         
         
@@ -128,13 +128,133 @@ class SplashViewController: UIViewController {
             dispatch_get_main_queue(), closure)
     }
 
-    
-    func addProPic(){
-        
-        
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if continuable {
+            print("Populating images")
+            populateImages()
+            let location = touches.first?.locationInView(self.view)
+            let h = Int((location?.x)! / imageSize)
+            let v = Int((location?.y)! / imageSize)
+            print("h - \(h)  v - \(v)")
+
+            rippleTransitionSetup(imgViews, h: h, v: v, iteration: 0)
+            
+            for v in 0...vertPics - 1 {
+                for h in 0...horzPics - 1 {
+                    
+//                    print("h - \(h)  v - \(v) tag - \(imgViews[v][h].tag - 1000)")
+                    rippleFade2(imgViews[v][h], delays: Double(imgViews[v][h].tag - 1000) * 0.1)
+                    
+                }
+            }
+        }
         
     }
+    
+    var images = Array<Array<UIImage>>()
+    var imgViews = Array<Array<UIImageView>>()
+    var fullImage: UIImage?
+    var horzPics = 0
+    var vertPics = 0
+    let imageSize = CGFloat(20.0)
+    
+    func populateImages(){
+        
+        
+        let size = self.view.frame.size
+        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.mainScreen().scale)
+        let rec = CGRectMake(0, 0, size.width, size.height)
+        self.view.drawViewHierarchyInRect(rec, afterScreenUpdates: true)
+        fullImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        horzPics = Int(ceil(size.width / imageSize))
+        vertPics = Int(ceil(size.height / imageSize))
+        
+        images = Array<Array<UIImage>>()
+        imgViews = Array<Array<UIImageView>>()
+        for _ in 0...vertPics - 1 {
+            images.append(Array(count: horzPics, repeatedValue: UIImage()))
+            imgViews.append(Array(count: horzPics, repeatedValue: UIImageView()))
+        }
+        
+        for v in 0...vertPics - 1 {
+            for h in 0...horzPics - 1 {
+                images[v][h] = croppedImage(fullImage!, cropRect: CGRectMake(CGFloat(h) * imageSize * UIScreen.mainScreen().scale,CGFloat(v) * imageSize * UIScreen.mainScreen().scale,imageSize * UIScreen.mainScreen().scale,imageSize * UIScreen.mainScreen().scale))
+                
+            }
+        }
+        view.subviews.forEach({ $0.removeFromSuperview() })
+        for v in 0...vertPics - 1 {
+            for h in 0...horzPics - 1 {
+                let imgView = UIImageView(frame: CGRectMake(CGFloat(h) * imageSize, CGFloat(v) * imageSize , images[v][h].size.width / UIScreen.mainScreen().scale, images[v][h].size.height / UIScreen.mainScreen().scale))
 
+                self.view.addSubview(imgView)
+                imgView.image = images[v][h]
+                imgViews[v][h] = imgView
+                imgView.alpha = 1.0
+            }
+        }
+        
+    }
+    
+    func croppedImage(image: UIImage, cropRect: CGRect) -> UIImage {
+        let imageRef = CGImageCreateWithImageInRect(image.CGImage, cropRect)
+        return UIImage(CGImage: imageRef!)
+    }
+    
+    func rippleTransitionSetup(images: [[UIImageView]], h: Int, v: Int, iteration:Int){
+        if (h < 0 || h >= images.first?.count || v < 0 || v >= images.count){
+            return
+        }
+        if (images[v][h].tag >= 1000){
+            if 1000 + iteration < images[v][h].tag {
+                images[v][h].tag = 1000 + iteration
+            }else{
+                return
+            }
+        }else{
+            images[v][h].tag = 1000 + iteration
+        }
+//        print("h - \(h)  v - \(v) delay - \(Double(iteration) * 0.1)")
+//        rippleFade(images[v][h], delays: Double(iteration) * 0.1)
+        
+        rippleTransitionSetup(images, h: h - 1, v: v, iteration: iteration + 1)
+        rippleTransitionSetup(images, h: h + 1, v: v, iteration: iteration + 1)
+        rippleTransitionSetup(images, h: h, v: v - 1, iteration: iteration + 1)
+        rippleTransitionSetup(images, h: h, v: v + 1, iteration: iteration + 1)
+    }
+    
+    func rippleFade(image : UIImageView, delays : Double) {
+
+//        UIView.animateWithDuration(delays, animations: { () -> Void in
+//            
+//            }) { (finished) -> Void in
+//                UIView.animateWithDuration(1.0, animations: { () -> Void in
+//                    image.alpha = 0.0
+//                })
+//        }
+        delay(delays) { () -> () in
+            UIView.animateWithDuration(1.0, animations: { () -> Void in
+                image.alpha = 0.0
+            })
+        }
+    }
+    
+    func rippleFade2(image : UIImageView, delays : Double) {
+        delay(delays) { () -> () in
+            
+            UIView.animateWithDuration(1.0, animations: { () -> Void in
+                image.transform = CGAffineTransformMakeScale(0.15, 0.15)
+                }, completion: { (finished) -> Void in
+                    UIView.animateWithDuration(0.8, animations: { () -> Void in
+                        image.alpha = 0.0
+                    })
+            })
+            
+        }
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
