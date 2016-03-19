71b16fd047e3d1f1dc0a874ab58df7f5139a9bc9
Avery Lamp <averylamp@gmail.com>
2016-03-19 06:02:44 -0400

    unfolding cell now working

---
+++ Avery Lamp/ExpandingCell.swift
@@ -0,0 +1,40 @@
+//
+//  ExpandingCell.swift
+//  Avery Lamp
+//
+//  Created by Avery Lamp on 3/19/16.
+//  Copyright © 2016 Avery Lamp. All rights reserved.
+//
+
+import UIKit
+import FoldingCell
+
+class ExpandingCell: FoldingCell {
+
+    
+    @IBOutlet var highlightViews: [UIView]!
+    
+    override func awakeFromNib() {
+        foregroundView.layer.cornerRadius = 10
+        foregroundView.layer.masksToBounds = true
+        foregroundView.clipsToBounds = true
+        contentView.layer.cornerRadius = 10
+
+        highlightViews.forEach{ $0.backgroundColor = UIColor.orangeColor()}
+        super.awakeFromNib()
+    }
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+     override func animationDuration(itemIndex:NSInteger, type:AnimationType)-> NSTimeInterval {
+        
+        // durations count equal it itemCount
+        let durations = [0.33, 0.26, 0.26, 0.33, 0.33] // timing animation for each view
+        return durations[itemIndex]
+    }
+
+}
---
+++ Avery Lamp/MyAppsTableViewController.swift
@@ -0,0 +1,144 @@
+//
+//  MyAppsTableViewController.swift
+//  Avery Lamp
+//
+//  Created by Avery Lamp on 3/19/16.
+//  Copyright © 2016 Avery Lamp. All rights reserved.
+//
+
+import UIKit
+import FoldingCell
+
+class MyAppsTableViewController: UITableViewController {
+
+    
+    let kCloseCellHeight: CGFloat = 168
+    let kOpenCellHeight: CGFloat = 648
+    let kRowsCount = 6
+    var cellHeights = [CGFloat]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        for _ in 0...kRowsCount {
+            cellHeights.append(kCloseCellHeight)
+        }
+        tableView.backgroundColor = UIColor(red: 0.671, green: 0.910, blue: 0.965, alpha: 1.00)
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as! FoldingCell
+        
+        var duration = 0.0
+        if cellHeights[indexPath.row] == kCloseCellHeight { // open cell
+            cellHeights[indexPath.row] = kOpenCellHeight
+            cell.selectedAnimation(true, animated: true, completion: nil)
+            duration = 0.5
+        } else {// close cell
+            cellHeights[indexPath.row] = kCloseCellHeight
+            cell.selectedAnimation(false, animated: true, completion: nil)
+            duration = 1.1
+        }
+        
+        UIView.animateWithDuration(duration, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            }, completion: nil)
+    }
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if cell is FoldingCell {
+            let foldingCell = cell as! FoldingCell
+            
+            if cellHeights[indexPath.row] == kCloseCellHeight {
+                foldingCell.selectedAnimation(false, animated: false, completion:nil)
+            } else {
+                foldingCell.selectedAnimation(true, animated: false, completion: nil)
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return cellHeights[indexPath.row]
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return kRowsCount
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FoldingCell", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.clearColor()
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
